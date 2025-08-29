b00c1135dc931171e6aec9b0705f5864de62e428
MrXYZ <160528639+LoveSpark-IIITM@users.noreply.github.com>
2025-08-29 17:59:41 +0530

    Update MongoDb.js

--- MongoDb.js
+++ MongoDb.js
@@ -1,11 +1,7 @@
-const { MongoClient, ObjectId } = require('mongodb');
+// MongoDb.js
+import { MongoClient, ObjectId } from "mongodb";
 
-class MongoDBConnection {
-  /**
-   * Initialize MongoDB connection.
-   * @param {string} connectionString - MongoDB connection URI.
-   * @param {string} dbName - Name of the database.
-   */
+export default class MongoDBConnection {
   constructor(connectionString, dbName) {
     this.connectionString = connectionString;
     this.dbName = dbName;
@@ -13,120 +9,47 @@ class MongoDBConnection {
     this.db = null;
   }
 
-  /**
-   * Establish a connection to MongoDB.
-   */
   async connect() {
-    try {
-      this.client = new MongoClient(this.connectionString);
-      await this.client.connect();
-      this.db = this.client.db(this.dbName);
-      console.log('Successfully connected to MongoDB!');
-    } catch (error) {
-      console.error('Failed to connect to MongoDB:', error);
-      throw error;
-    }
+    this.client = new MongoClient(this.connectionString);
+    await this.client.connect();
+    this.db = this.client.db(this.dbName);
+    console.log("MongoDB connected!");
   }
 
-  /**
-   * Close the MongoDB connection.
-   */
   async close() {
-    try {
-      if (this.client) {
-        await this.client.close();
-        console.log('MongoDB connection closed.');
-      }
-    } catch (error) {
-      console.error('Error closing MongoDB connection:', error);
-      throw error;
-    }
+    if (this.client) await this.client.close();
+    console.log("MongoDB connection closed.");
   }
 
-  /**
-   * Insert a single document into a collection.
-   * @param {string} collectionName - Name of the collection.
-   * @param {Object} document - Document to insert.
-   * @returns {string} The inserted document's ID.
-   */
   async insertOne(collectionName, document) {
-    if (!this.db) {
-      throw new Error('Database not connected.');
-    }
-    const collection = this.db.collection(collectionName);
-    const result = await collection.insertOne(document);
+    if (!this.db) throw new Error("DB not connected");
+    const result = await this.db.collection(collectionName).insertOne(document);
     return result.insertedId.toString();
   }
 
-  /**
-   * Find a single document in a collection.
-   * @param {string} collectionName - Name of the collection.
-   * @param {Object} query - Query to filter documents.
-   * @returns {Object|null} The found document or null.
-   */
   async findOne(collectionName, query = {}) {
-  if (!this.db) {
-    throw new Error('Database not connected.');
+    if (!this.db) throw new Error("DB not connected");
+    const doc = await this.db.collection(collectionName).findOne(query);
+    if (doc && doc._id) doc._id = doc._id.toString();
+    return doc;
   }
-  const collection = this.db.collection(collectionName);
-  const document = await collection.findOne(query);
-  if (document && document._id) {
-    document._id = document._id.toString();
-  }
-  return document;
-}
 
-
-  /**
-   * Find multiple documents in a collection.
-   * @param {string} collectionName - Name of the collection.
-   * @param {Object} query - Query to filter documents.
-   * @returns {Array} List of found documents.
-   */
   async findMany(collectionName, query = {}) {
-    if (!this.db) {
-      throw new Error('Database not connected.');
-    }
-    const collection = this.db.collection(collectionName);
-    const documents = await collection.find(query).toArray();
-    documents.forEach((doc) => {
-      if (doc._id) {
-        doc._id = doc._id.toString();
-      }
-    });
-    return documents;
+    if (!this.db) throw new Error("DB not connected");
+    const docs = await this.db.collection(collectionName).find(query).toArray();
+    docs.forEach((d) => d._id && (d._id = d._id.toString()));
+    return docs;
   }
 
-  /**
-   * Update a single document in a collection.
-   * @param {string} collectionName - Name of the collection.
-   * @param {Object} query - Query to filter documents.
-   * @param {Object} update - Update to apply.
-   * @returns {boolean} True if the update was successful, false otherwise.
-   */
   async updateOne(collectionName, query, update) {
-    if (!this.db) {
-      throw new Error('Database not connected.');
-    }
-    const collection = this.db.collection(collectionName);
-    const result = await collection.updateOne(query, { $set: update });
-    return result.modifiedCount > 0;
+    if (!this.db) throw new Error("DB not connected");
+    const res = await this.db.collection(collectionName).updateOne(query, { $set: update });
+    return res.modifiedCount > 0;
   }
 
-  /**
-   * Delete a single document from a collection.
-   * @param {string} collectionName - Name of the collection.
-   * @param {Object} query - Query to filter documents.
-   * @returns {boolean} True if the deletion was successful, false otherwise.
-   */
   async deleteOne(collectionName, query) {
-    if (!this.db) {
-      throw new Error('Database not connected.');
-    }
-    const collection = this.db.collection(collectionName);
-    const result = await collection.deleteOne(query);
-    return result.deletedCount > 0;
+    if (!this.db) throw new Error("DB not connected");
+    const res = await this.db.collection(collectionName).deleteOne(query);
+    return res.deletedCount > 0;
   }
 }
-
-module.exports = MongoDBConnection;
